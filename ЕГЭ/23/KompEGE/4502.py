6a4c36fe0df45ec69a615fe20a5fec2df08ccf1b
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-03-03 20:52:31 +0700

    upd

--- ЕГЭ/23/KompEGE/4502.py
+++ ЕГЭ/23/KompEGE/4502.py
@@ -4,7 +4,7 @@ def f(n=1, comm_num=0):
     if comm_num == 6:
         if 34 <= n <= 59:
             nums.add(n)
-        return # None
+        return
     
     return f(n + 1, comm_num + 1) or f(n + 2, comm_num + 1) or f(n*2, comm_num + 1)
 
